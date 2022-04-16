fdf7fc3b581cc14cb6d1ed9a4bcf25e50c37c493
Krishna kant <kkujjawal2002@gmail.com>
2022-04-17 04:44:58 +0530

    some minor changes in UI

--- js/end.js
+++ js/end.js
@@ -1,23 +1,28 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+
+// get the final score
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+// get highest score from your localStorage
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const MAX_HIGH_SCORES = 5;
+
+// update final Score with most recent score.
 finalScore.innerText = mostRecentScore;
-username.addEventListener('keyup',()=>{
-    saveScoreBtn.disabled = !username.value;
-})
-saveHighScore = (e)=>{
-    
-    e.preventDefault();
-    const score ={
-        score: mostRecentScore,
-        name:username.value
-    };
-    highScores.push(score);
-    highScores.sort((a,b)=>b.score - a.score);
-    highScores.splice(5);
-    localStorage.setItem('highScores',JSON.stringify(highScores));
-    window.location.assign("/html/");
-}
+username.addEventListener("keyup", () => {
+  saveScoreBtn.disabled = !username.value;
+});
+saveHighScore = (e) => {
+  e.preventDefault();
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/html/");
+};
